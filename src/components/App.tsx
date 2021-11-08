a9028869a4f44f516ef56d49dc91ae1bb19de908
Eduardo Bravo <eduardo.bravo@cognizant.com>
2021-11-08 17:18:47 +0100

    Button styled component

---
+++ src/components/App.tsx
@@ -0,0 +1,14 @@
+import React, { Fragment } from 'react';
+
+import { Button } from 'components/common';
+
+function App() {
+  return (
+    <Fragment>
+      <h1>App</h1>
+      <Button>Test</Button>
+    </Fragment>
+  );
+}
+
+export default App;
